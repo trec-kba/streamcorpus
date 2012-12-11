9834653a149381004e9915f65f9929207c7291bf
John R. Frank <jrf@mit.edu>
2012-12-11 01:58:17 -0500

    This commit makes an initial set of utilities for StreamCorpus.
    As noted in the comments of the Makefile, this pushes the results of
    thrift code generation into our python package.
    
    	## The 'right' thing to do here is apparently to use the
    	## 'namespace' functionality in python setup tools.  However,
    	## this is not yet adopted into core python and has some
    	## kludgy sounding stuff about using __init__.py for the sole
    	## purpose of causing (tricking?) python to looking further
    	## through sys.path if it fails to find a component in the
    	## first module with the requested name.  Instead of taking a
    	## depency on that, we maintain control of the kludge here in
    	## this Makefile by copying the code files generated by
    	## thrift into our working tree and not the empty __init__.py
    	## files from thrift.
    
    The setup.py successfully builds and installs the StreamCorpus
    package, however it does not yet create RPMs.  Looks like need to add
    this to Makefile to make cleanup repeatable.
    
    The main content in this commit is in src/StreamCorpus/__init__.py and
    src/StreamCorpus.thrift which has significant changes compared with
    the original kba.thrift design.  Moved the non-body ContentItems into
    a dict, and similarly moved SourceMetadata into a dict.
    
    The __init__.py file in StreamCorpus introduces the notion of a Chunk
    and offers convenience methods on chunks.  This is not yet totally
    pythonic but getting closer.
    
    This does not yet exercise Labels and Offsets.  That's part of what is
    next.

---
+++ setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import fnmatch
+
+## prepare to run PyTest as a command
+from distutils.core import Command
+
+## explain this...
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+PROJECT = 'StreamCorpus'
+VERSION = '0.1'
+URL = 'http://github.com/trec-kba/kba-corpus'
+AUTHOR = 'John R. Frank'
+AUTHOR_EMAIL = 'jrf@diffeo.com'
+DESC = 'Tools for organizing a collections of text for entity-centric stream processing.'
+
+def read_file(file_name):
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        file_name
+        )
+    return open(file_path).read()
+
+def recursive_glob(treeroot, pattern):
+    results = []
+    for base, dirs, files in os.walk(treeroot):
+      goodfiles = fnmatch.filter(files, pattern)
+      results.extend(os.path.join(base, f) for f in goodfiles)
+    return results
+
+class PyTest(Command):
+    '''run py.test'''
+
+    description = 'runs py.test to execute all tests'
+
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description=DESC,
+    long_description=read_file('README.md'),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    cmdclass = {'test': PyTest},
+    # We can select proper classifiers later
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: MIT',  ## MIT/X11 license http://opensource.org/licenses/MIT
+    ],
+    install_requires=[
+        'thrift',
+    ],
+    # include_package_data = True,
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        # '': ['*.txt', '*.rst'],
+        # And include any files found in the 'data' package:
+        # '': recursive_glob('src/data/', '*')
+        '': recursive_glob('data/', '*')
+    }
+)
