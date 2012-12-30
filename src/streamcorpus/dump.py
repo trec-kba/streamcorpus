67d581b5064fe6c246d7ed20af3ec2c568ad05ba
John R. Frank <jrf@computableinsights.com>
2012-12-30 09:28:04 -0500

    refactoring dump to handle streaming file names over stdin and generating counts of messages in Chunks

--- src/streamcorpus/dump.py
+++ src/streamcorpus/dump.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 '''
-Reads in a streamcorpus.Chunk file and prints the metadata of each
-StreamItem to stdout
+A command line utility for printing out information from
+streamcorpus.Chunk files.
+
+This software is released under an MIT/X11 open source license.
+
+Copyright 2012 Diffeo, Inc.
 '''
 
+import os
+import sys
 from ._chunk import Chunk
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_path')
-    parser.add_argument('--show-content-items', action='store_true', 
-                        default=False, dest='show_content_items')
-    parser.add_argument('--tokens-only', action='store_true', 
-                        default=False, dest='tokens_only')
-    args = parser.parse_args()
+def _dump(fpath, args):
+    '''
+    Reads in a streamcorpus.Chunk file and prints the metadata of each
+    StreamItem to stdout
+    '''
+    if args.count:
+        num = 0
 
-    for si in Chunk(file_obj=open(args.input_path)):
+    for si in Chunk(path=fpath, mode='rb'):
+        if args.count:
+            num += 1
+            continue
         if args.tokens_only:
             if si.body.sentences:
                 for tagger_id in si.body.sentences:
@@ -31,3 +38,32 @@ if __name__ == '__main__':
                 for oc in si.other_content:
                     si.other_content[oc] = None
         print si
+
+    if args.count:
+        print num
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'input_path', 
+        help='Path to a single chunk file, or directory of chunks, or "-" for receiving paths over stdin')
+    parser.add_argument('--show-content-items', action='store_true', 
+                        default=False, dest='show_content_items')
+    parser.add_argument('--count', action='store_true', 
+                        default=False, help='print number of StreamItems')
+    parser.add_argument('--tokens-only', action='store_true', 
+                        default=False, dest='tokens_only')
+    args = parser.parse_args()
+
+    if args.input_path == '-':
+        for fpath in sys.stdin:
+            _dump(fpath.strip(), args)
+
+    elif os.path.isdir(args.input_path):
+        for fname in os.listdir(args.input_path):
+            fpath = os.path.join(args.input_path, fname)
+            _dump(fpath, args)
+
+    else:
+        _dump(args.input_path, args)
