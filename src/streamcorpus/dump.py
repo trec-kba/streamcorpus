a3279d95593068759040ca6a325e9a6c70e3d9f1
John R. Frank <jrf@diffeo.com>
2013-01-11 16:27:28 -0500

    making a stable dump format that can be used with diff in tests

--- src/streamcorpus/dump.py
+++ src/streamcorpus/dump.py
@@ -57,12 +57,49 @@ def _dump(fpath, args):
     if args.count:
         print '%d\t%d\t%s' % (num_stream_items, len(num_labeled_stream_items), fpath)
 
+token_attrs = [
+    'token_num',
+    'sentence_pos',
+    'token',
+    'offsets',
+    'lemma',
+    'pos',
+    'entity_type',
+    'mention_id',
+    'equiv_id',
+    'parent_id',
+    'dependency_path',
+    ]
+
+def _dump_tokens(fpaths):
+    '''
+    Read in a streamcorpus.Chunk file and print all tokens in a fixed
+    order that enables diffing.
+    '''
+    print '\t'.join(token_attrs + ['stream_id', 'labels'])
+    for fpath in fpaths:
+        for si in Chunk(path=fpath, mode='rb'):
+            if not si.body.sentences:
+                continue
+            tagger_ids = si.body.sentences.keys()
+            tagger_ids.sort()
+            for tagger_id in tagger_ids:
+                for sent in si.body.sentences[tagger_id]:
+                    for tok in sent.tokens:
+                        vals = [repr(getattr(tok, attr))
+                                for attr in token_attrs]
+                        vals += [si.stream_id]
+                        vals += [','.join([label.target_id for label in tok.labels])]
+                        print '\t'.join(vals)
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'input_path', 
         help='Path to a single chunk file, or directory of chunks, or "-" for receiving paths over stdin')
+    parser.add_argument('--tokens', action='store_true', 
+                        default=False, dest='tokens')
     parser.add_argument('--show-all', action='store_true', 
                         default=False, dest='show_all')
     parser.add_argument('--show-content-field', 
@@ -73,14 +110,21 @@ if __name__ == '__main__':
                         default=False, dest='labels_only')
     args = parser.parse_args()
 
+
+    ## make input_path into an iterable over path strings
     if args.input_path == '-':
-        for fpath in sys.stdin:
-            _dump(fpath.strip(), args)
+        args.input_path = itertools.imap(lambda line: line.strip, sys.stdin)
 
     elif os.path.isdir(args.input_path):
-        for fname in os.listdir(args.input_path):
-            fpath = os.path.join(args.input_path, fname)
-            _dump(fpath, args)
+        args.input_path = itertools.imap(lambda fname: os.path.join(args.input_path, fname),
+                                         os.listdir(args.input_path))
 
     else:
-        _dump(args.input_path, args)
+        args.input_path = [args.input_path]
+
+    if args.tokens:
+        _dump_tokens(args.input_path)
+
+    else:
+        for fpath in args.input_path:
+            _dump(fpath, args)
