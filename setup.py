53670a1e4be6ec978f6c442bb4ee84501d7ed55d
John R. Frank <jrf@diffeo.com>
2013-02-10 01:35:19 -0500

    bumping version number to reflect improved diagnostics

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import Command
 from setuptools import setup, find_packages
 
 PROJECT = 'streamcorpus'
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 URL = 'http://github.com/trec-kba/kba-corpus'
 AUTHOR = 'Diffeo, Inc.'
 AUTHOR_EMAIL = 'support@diffeo.com'
