2be21a221275c92dc88f245f879139b46e86bc62
Jacek R. Ambroziak <jacek.ambroziak@gmail.com>
2013-03-08 16:20:31 -0500

    added the Java part

---
+++ scala/src/test/scala/streamcorpus/Test.scala
@@ -0,0 +1,22 @@
+import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.transport.TFileTransport
+import streamcorpus.StreamItem
+
+/**
+ * Date: 3/7/13
+ * Time: 12:31 PM
+ */
+
+object Test {
+  def main(args: Array[String]) {
+    val transport: TFileTransport = new TFileTransport("/tmp/john-smith-tagged-by-lingpipe-0.sc", true)
+    val binProto: TBinaryProtocol = new TBinaryProtocol(transport)
+    transport.open()
+    println(binProto)
+    while (true) {
+      val imm5: StreamItem.Immutable = StreamItem.decode(binProto)
+      println(imm5)
+
+    }
+  }
+}
