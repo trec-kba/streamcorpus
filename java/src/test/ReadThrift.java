f3ff62be77fa09a6fc089ab2c531518fa0da3784
Jacek R. Ambroziak <jacek.ambroziak@gmail.com>
2013-03-08 16:34:05 -0500

    shortened test example to essentials

--- java/src/test/ReadThrift.java
+++ java/src/test/ReadThrift.java
@@ -1,13 +1,8 @@
 package test;
 
-import org.apache.thrift.TBase;
-import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TFileTransport;
 import streamcorpus.StreamItem;
-
-import java.io.IOException;
 
 /**
  * User: jacek
@@ -15,48 +10,15 @@ import java.io.IOException;
  * Time: 3:38 PM
  */
 public final class ReadThrift {
-    /**
-     * Used to create empty objects that will be initialized with values from the file.
-     */
-    protected final TBaseCreator creator;
-
-    public ReadThrift(final TBaseCreator creator) {
-        this.creator = creator;
-    }
-
-    public static interface TBaseCreator {
-        TBase create();
-    }
-
-    /**
-     * Reads the next object from the file.
-     */
-    public TBase read(TProtocol binaryIn) throws IOException {
-        TBase t = creator.create();
-        try {
-            t.read(binaryIn);
-        } catch (TException e) {
-            throw new IOException(e);
-        }
-        return t;
-    }
-
     public static void main(String[] args) {
-
-        ReadThrift readThrift = new ReadThrift(new TBaseCreator() {
-            @Override
-            public TBase create() {
-                return new StreamItem();
-            }
-        });
-
         try {
             TFileTransport transport = new TFileTransport("test-data/john-smith-tagged-by-lingpipe-0.sc", true);
-            TBinaryProtocol protocol = new TBinaryProtocol(transport);
             transport.open();
+
+            TBinaryProtocol protocol = new TBinaryProtocol(transport);
             int counter = 0;
             while (true) {
-                StreamItem item = new StreamItem();
+                final StreamItem item = new StreamItem();
                 item.read(protocol);
                 System.out.println("counter = " + ++counter);
                 System.out.println("item = " + item);
