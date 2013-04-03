774d2214fa5cec8bb84b0615fdffe6b6a182dcf4
Jacek R. Ambroziak <jacek.ambroziak@gmail.com>
2013-04-03 11:03:50 -0400

    expanded Test example to iterate over tokens

--- scala/src/test/scala/streamcorpus/Test.scala
+++ scala/src/test/scala/streamcorpus/Test.scala
@@ -1,23 +1,35 @@
 import java.io.FileInputStream
 import org.apache.thrift.protocol.TBinaryProtocol
-import org.apache.thrift.transport.{TIOStreamTransport, TTransport, TFileTransport}
-import streamcorpus.StreamItem
-
-/**
- * Date: 3/7/13
- * Time: 12:31 PM
- */
+import org.apache.thrift.transport.{TTransportException, TIOStreamTransport, TTransport}
+import streamcorpus.{StreamItem}
 
 object Test {
   def main(args: Array[String]) {
     val transport: TTransport = new TIOStreamTransport(new FileInputStream("test-data/john-smith-tagged-by-lingpipe-0.sc"))
     val binProto: TBinaryProtocol = new TBinaryProtocol(transport)
     transport.open()
-    println(binProto)
     while (true) {
-      val imm5: StreamItem.Immutable = StreamItem.decode(binProto)
-      println(imm5)
-
+      try {
+        StreamItem.decode(binProto).body match {
+          case Some(contentItem) => {
+            for ((k, sentences) <- contentItem.sentences) {
+              println(k)
+              for (sentence <- sentences; token <- sentence.tokens) {
+                println(token.token)
+              }
+            }
+          }
+          case _ => println("DUNNO")
+        }
+        println("--------------------------------------------")
+      } catch {
+        case te: TTransportException => if (te.getType == TTransportException.END_OF_FILE) {
+          println("END")
+          return
+        } else {
+          println("type " + te.getType)
+        }
+      }
     }
   }
 }
