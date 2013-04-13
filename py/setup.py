7b2127d56fec7e1083837e1daf8b0d5c3a5e0b08
John R. Frank <jrf@computableinsights.com>
2013-04-13 13:59:36 -0400

    bumping python package version to v0.3.1

--- py/setup.py
+++ py/setup.py
@@ -14,7 +14,7 @@ from distutils.core import Command
 from setuptools import setup, find_packages
 
 PROJECT = 'streamcorpus'
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 URL = 'http://github.com/trec-kba/kba-corpus'
 AUTHOR = 'Diffeo, Inc.'
 AUTHOR_EMAIL = 'support@diffeo.com'
