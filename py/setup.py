88128dad389316b5b459a168152373b576039f7b
John R. Frank <jrf@diffeo.com>
2013-04-14 20:26:31 -0400

    bumping version number for patch on .seek issue

--- py/setup.py
+++ py/setup.py
@@ -14,7 +14,7 @@ from distutils.core import Command
 from setuptools import setup, find_packages
 
 PROJECT = 'streamcorpus'
-VERSION = '0.2.19'
+VERSION = '0.2.20'
 URL = 'http://github.com/trec-kba/kba-corpus'
 AUTHOR = 'Diffeo, Inc.'
 AUTHOR_EMAIL = 'support@diffeo.com'
