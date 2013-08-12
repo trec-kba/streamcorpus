d8a9a3a8420c3f3774e130349e35fb80f8de8ad6
John R. Frank <jrf@diffeo.com>
2013-08-12 17:06:49 +0000

    moving AddTagging.scala out of the streamcorpus package and into the examples directory

--- examples/scala/AddTagging.scala
+++ examples/scala/AddTagging.scala
@@ -1,29 +1,37 @@
-import java.io.FileInputStream
-import org.apache.thrift.protocol.TBinaryProtocol
-import org.apache.thrift.transport.{TTransportException, TIOStreamTransport, TTransport}
-import streamcorpus.{StreamItem}
+package test.scala.streamcorpus
 
-object Test {
+import org.apache.thrift.transport.{TTransportException, TIOStreamTransport, TTransport}
+import java.io.{FileOutputStream, FileInputStream}
+import org.apache.thrift.protocol.TBinaryProtocol
+import streamcorpus.{Token, Sentence, StreamItem}
+
+/**
+ * User: jacek
+ * Date: 8/10/13
+ * Time: 3:20 PM
+ */
+
+object AddTagging {
   def main(args: Array[String]) {
-    val transport: TTransport = new TIOStreamTransport(new FileInputStream("../test-data/john-smith-tagged-by-lingpipe-0.sc"))
-    val binProto: TBinaryProtocol = new TBinaryProtocol(transport)
-    transport.open()
+    val i_transport: TTransport = new TIOStreamTransport(new FileInputStream("../test-data/john-smith-tagged-by-lingpipe-0-v0_3_0.sc"))
+    val o_transport: TTransport = new TIOStreamTransport(new FileOutputStream("/tmp/john-smith-tagged-by-lingpipe-0-v0_3_0.sc"))
+    val i_binProto: TBinaryProtocol = new TBinaryProtocol(i_transport)
+    val o_binProto: TBinaryProtocol = new TBinaryProtocol(o_transport)
+    i_transport.open()
     while (true) {
       try {
-        StreamItem.decode(binProto).body match {
+        val item: StreamItem = StreamItem.decode(i_binProto)
+        item.body match {
           case Some(contentItem) => {
-            for ((k, sentences) <- contentItem.sentences) {
-              println(k)
-              for (sentence <- sentences; token <- sentence.tokens) {
-                println(token.token)
-              }
-            }
+            contentItem.sentences + ("my_tagger" -> Seq(Sentence(Seq(Token(0, "The"), Token(1, "cat")))))
           }
-          case _ => println("DUNNO")
+          case None => println("no content")
         }
-        println("--------------------------------------------")
+        item.write(o_binProto)
       } catch {
         case te: TTransportException => if (te.getType == TTransportException.END_OF_FILE) {
+          i_transport.close()
+          o_transport.close()
           println("END")
           return
         } else {
