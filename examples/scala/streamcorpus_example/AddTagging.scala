f4a0dcaa9354b6db620fb0b6acd97dc36fabfe56
Jacek R. Ambroziak <jacek.ambroziak@gmail.com>
2013-08-13 19:04:12 -0400

    copied immutable StreamItem to new, updated StreamItem for thrift encoding

--- examples/scala/streamcorpus_example/AddTagging.scala
+++ examples/scala/streamcorpus_example/AddTagging.scala
@@ -22,42 +22,49 @@ object AddTagging {
     i_transport.open()
     while (true) {
       try {
-	// deserialize the StreamItem from input transport
+        // deserialize the StreamItem from input transport
         val item: StreamItem = StreamItem.decode(i_protocol)
-	si_count += 1
+        si_count += 1
         item.body match {
           case Some(contentItem) => {
 
-	    // taggings is optional
-            contentItem.taggings + 
-	      ("my_tagger" -> Some(Tagging(
-	        taggerId = "my_tagger",
-                rawTagging = ByteBuffer.wrap(Array[Byte]()),  // serialized tagging data in some "native" format, such as XML or JSON
-	        taggerConfig = Option("some description"),
+            // taggings is optional
+            contentItem.taggings +
+              ("my_tagger" -> Some(Tagging(
+                taggerId = "my_tagger",
+                rawTagging = ByteBuffer.wrap(Array[Byte]()), // serialized tagging data in some "native" format, such as XML or JSON
+                taggerConfig = Option("some description"),
                 taggerVersion = Option("v0.0.infinity"),
-                generationTime = Some(StreamTime(zuluTimestamp="1970-01-01T00:00:01.000000Z", epochTicks=1))
-              )));
+                generationTime = Some(StreamTime(zuluTimestamp = "1970-01-01T00:00:01.000000Z", epochTicks = 1))
+              )))
 
-            contentItem.sentences +
-	      "my_tagger" -> Seq(Sentence(tokens = Seq(
-	          Token(0, "The"), 
-		  Token(1, "cat", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId=1),
-		  Token(2, "ate"),
-		  Token(3, "the"),
-		  Token(4, "bird", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId=2),
-		  Token(5, ".")  // a tagger can define whatever tokenization and sentence chunking it likes
-              )));
+            val newMap = contentItem.sentences + (
+              "my_tagger" -> Seq(Sentence(tokens = Seq(
+                Token(0, "The"),
+                Token(1, "cat", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 1),
+                Token(2, "ate"),
+                Token(3, "the"),
+                Token(4, "bird", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 2),
+                Token(5, ".") // a tagger can define whatever tokenization and sentence chunking it likes
+              ))))
 
-            //contentItem.relations + ("my_tagger" -> Seq(Relation(
+            val newItem = item.copy(body = Some(contentItem.copy(sentences = newMap)))
 
-	    println(contentItem.sentences)
-	    println(contentItem.sentences.contains("my_tagger"))
+            newItem.body match {
+              case Some(contentItem) => {
+                println(contentItem.sentences.contains("my_tagger"))
+              }
+              case _ =>
+            }
 
+            newItem.write(o_protocol)
           }
-          case None => println("no content")
+          case None => {
+            println("no content")
+            item.write(o_protocol)
+          }
         }
-	// serialize the item to the output transport
-        item.write(o_protocol)
+        // serialize the item to the output transport
       } catch {
         case te: TTransportException => if (te.getType == TTransportException.END_OF_FILE) {
           i_transport.close()
