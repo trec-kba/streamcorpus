b14e3e806ac965e45498f5fe239b37a854913e5f
John R. Frank <jrf@diffeo.com>
2013-08-13 18:38:37 +0000

    trying to count streamitems...

--- examples/scala/streamcorpus_example/AddTagging.scala
+++ examples/scala/streamcorpus_example/AddTagging.scala
@@ -15,24 +15,28 @@ object AddTagging {
   def main(args: Array[String]) {
     val i_transport: TTransport = new TIOStreamTransport(new FileInputStream("../../test-data/john-smith-tagged-by-lingpipe-0-v0_3_0.sc"))
     val o_transport: TTransport = new TIOStreamTransport(new FileOutputStream("/tmp/john-smith-tagged-by-lingpipe-0-v0_3_0.sc"))
-    val i_binProto: TBinaryProtocol = new TBinaryProtocol(i_transport)
-    val o_binProto: TBinaryProtocol = new TBinaryProtocol(o_transport)
+    val i_protocol: TBinaryProtocol = new TBinaryProtocol(i_transport)
+    val o_protocol: TBinaryProtocol = new TBinaryProtocol(o_transport)
+    val count: Int = 0
     i_transport.open()
     while (true) {
       try {
-        val item: StreamItem = StreamItem.decode(i_binProto)
+	// deserialize the StreamItem from input transport
+        val item: StreamItem = StreamItem.decode(i_protocol);
+	count = count + 1;
         item.body match {
           case Some(contentItem) => {
             contentItem.sentences + ("my_tagger" -> Seq(Sentence(Seq(Token(0, "The"), Token(1, "cat", entityType = Some(EntityType.Per))))))
           }
           case None => println("no content")
         }
-        item.write(o_binProto)
+	// serialize the item to the output transport
+        item.write(o_protocol)
       } catch {
         case te: TTransportException => if (te.getType == TTransportException.END_OF_FILE) {
           i_transport.close()
           o_transport.close()
-          println("END")
+          println("FINISHED writing " + count + " StreamItems")
           return
         } else {
           println("type " + te.getType)
