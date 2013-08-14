164c487c203fc892b14d9d8b292ddc9046195c11
John R. Frank <jrf@diffeo.com>
2013-08-14 02:42:56 +0000

    reverting back to not inline, for readability

--- examples/scala/streamcorpus_example/AddTagging.scala
+++ examples/scala/streamcorpus_example/AddTagging.scala
@@ -7,9 +7,8 @@ import org.apache.thrift.protocol.TBinaryProtocol
 import streamcorpus._
 
 /**
- * User: jacek
- * Date: 8/10/13
- * Time: 3:20 PM
+ * User: jacek, jrf
+ * Date: 8/13/13
  */
 
 object AddTagging {
@@ -27,30 +26,61 @@ object AddTagging {
         si_count += 1
         item.body match {
           case Some(contentItem) => {
-            item.copy(body = Some(contentItem.copy(
-              taggings = contentItem.taggings + (
-                "my_tagger" -> Tagging(
-                  taggerId = "my_tagger",
-                  rawTagging = ByteBuffer.wrap(Array[Byte]()), // serialized tagging data in some "native" format, such as XML or JSON
-                  taggerConfig = Option("some description"),
-                  taggerVersion = Option("v0.0.infinity"),
-                  generationTime = Some(StreamTime(zuluTimestamp = "1970-01-01T00:00:01.000000Z", epochTicks = 1))
-                )),
-              sentences = contentItem.sentences + (
-                "my_tagger" -> Seq(Sentence(tokens = Seq(
-                  Token(0, "The"),
-                  Token(1, "cat", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 1),
-                  Token(2, "ate"),
-                  Token(3, "the"),
-                  Token(4, "bird", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 2),
-                  Token(5, ".") // a tagger can define whatever tokenization and sentence chunking it likes
-                )))),
-              relations = contentItem.relations + (
-                "my_tagger" -> Seq(Relation(mentionId1 = Option(1), mentionId2 = Option(2), relationType = Option(RelationType.LifeInjure)))
-                )
-            ))).write(o_protocol)
+
+            // taggings is optional
+            val newTaggings = contentItem.taggings + (
+              "my_tagger" -> Tagging(
+                taggerId = "my_tagger",
+                rawTagging = ByteBuffer.wrap(Array[Byte]()), // serialized tagging data in some "native" format, such as XML or JSON
+                taggerConfig = Option("some description"),
+                taggerVersion = Option("v0.0.infinity"),
+                generationTime = Some(StreamTime(zuluTimestamp = "1970-01-01T00:00:01.000000Z", epochTicks = 1))
+              ))
+             val newSentences = contentItem.sentences + (
+	       "my_tagger" -> Seq(Sentence(tokens = Seq(
+                Token(0, "The"),
+                Token(1, "cat", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 1),
+                Token(2, "ate"),
+                Token(3, "the"),
+                Token(4, "bird", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 2),
+                Token(5, ".") // a tagger can define whatever tokenization and sentence chunking it likes
+              ))))
+
+            val newRelations = contentItem.relations + (
+              "my_tagger" -> Seq(Relation(mentionId1 = Option(1), mentionId2 = Option(2), relationType = Option(RelationType.LifeInjure)))
+              )
+
+            val newItem = item.copy(body = Some(contentItem.copy(
+              taggings = newTaggings,
+              sentences = newSentences,
+              relations = newRelations
+            )))
+
+            newItem.body match {
+              case Some(contentItem) => {
+
+	        // verify that the other sentences data was not destroyed
+                assert (contentItem.sentences.contains("lingpipe"), println("sentences lost lingpipe"))
+		// and that our tagging info got added
+                assert (contentItem.sentences.contains("my_tagger"), println("sentences missing my_tagger"))
+
+	        // verify similar things about taggings
+                assert (contentItem.taggings.contains("lingpipe"), println("taggings lost lingpipe"))
+                assert (contentItem.taggings.contains("my_tagger"), println("taggings missing my_tagger"))
+
+		// verify that relations has our data, there were not reln from lingpipe
+                assert (contentItem.relations.contains("my_tagger"), println("relations missing my_tagger"))
+
+                println("valid")
+
+              }
+              case _ =>
+            }
+
+            newItem.write(o_protocol)
           }
           case None => {
+            println("no content")
             item.write(o_protocol)
           }
         }
