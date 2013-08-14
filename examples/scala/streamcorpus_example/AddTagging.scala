cbf966aa78f3efd2e775cb9a52079b37825ae907
John R. Frank <jrf@diffeo.com>
2013-08-14 02:49:00 +0000

    re-indenting for readability, no code changes

--- examples/scala/streamcorpus_example/AddTagging.scala
+++ examples/scala/streamcorpus_example/AddTagging.scala
@@ -29,22 +29,58 @@ object AddTagging {
 
             // taggings is optional
             val newTaggings = contentItem.taggings + (
-              "my_tagger" -> Tagging(
-                taggerId = "my_tagger",
-                rawTagging = ByteBuffer.wrap(Array[Byte]()), // serialized tagging data in some "native" format, such as XML or JSON
-                taggerConfig = Option("some description"),
-                taggerVersion = Option("v0.0.infinity"),
-                generationTime = Some(StreamTime(zuluTimestamp = "1970-01-01T00:00:01.000000Z", epochTicks = 1))
-              ))
-             val newSentences = contentItem.sentences + (
-	       "my_tagger" -> Seq(Sentence(tokens = Seq(
-                Token(0, "The"),
-                Token(1, "cat", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 1),
-                Token(2, "ate"),
-                Token(3, "the"),
-                Token(4, "bird", entityType = Some(EntityType.Per), mentionType = Some(MentionType.Nom), mentionId = 2),
-                Token(5, ".") // a tagger can define whatever tokenization and sentence chunking it likes
-              ))))
+                "my_tagger" -> Tagging(
+                    taggerId = "my_tagger",
+                    rawTagging = ByteBuffer.wrap(Array[Byte]()), // serialized tagging data in some "native" format, such as XML or JSON
+                    taggerConfig = Option("some description"),
+                    taggerVersion = Option("v0.0.infinity"),
+                    generationTime = Some(StreamTime(zuluTimestamp = "1970-01-01T00:00:01.000000Z", epochTicks = 1))
+                ))
+
+            // Even though the streamcorpus-v0_3_0.thrift interface
+            // definition says entity_type and mention_id, etc. the
+            // Scrooge thrift compiler for Scala converts these to
+            // "entityType" to be more Scala-ish.  Not sure what
+            // happens with collisions in this re-writing scheme, but
+            // it seems to work for the streamcorpus interfaces, which
+            // are consistent with foo_bar naming for property names
+            // and FooBar naming of class names.  The resulting output
+            // is in the correct field, i.e. entity_type.  Scrooge
+            // also renames things in enums, so EntityType.PER becomes
+            // EntityType.Per.  This can take some experimentation to
+            // find the right spelling.
+            val newSentences = contentItem.sentences + (
+                "my_tagger" -> Seq(
+                    Sentence(tokens = Seq(
+                        Token(0, "The"),
+                        Token(1, "cat", 
+                              entityType = Some(EntityType.Per), 
+                              mentionType = Some(MentionType.Nom), 
+                              mentionId = 1,
+                              equivId = 1 // identifier for within-doc coref chain
+                          ),
+                        Token(2, "ate"),
+                        Token(3, "the"),
+                        Token(4, "bird", 
+                              entityType = Some(EntityType.Per), 
+                              mentionType = Some(MentionType.Nom), 
+                              mentionId = 2,
+                              equivId = 2
+                          ),
+                        Token(5, ".") // a tagger can define whatever tokenization and sentence chunking it likes
+                    )),
+                    Sentence(tokens = Seq(
+                        Token(0, "Bad"),
+                        Token(1, "kitty", entityType = Some(EntityType.Per), 
+                              mentionType = Some(MentionType.Nom), 
+                              mentionId = 3,
+                              equivId = 1 // identifier for within-doc coref chain (see above)
+                          ),
+                        Token(3, ".")
+                    )
+                )
+            ))
+
 
             val newRelations = contentItem.relations + (
               "my_tagger" -> Seq(Relation(mentionId1 = Option(1), mentionId2 = Option(2), relationType = Option(RelationType.LifeInjure)))
