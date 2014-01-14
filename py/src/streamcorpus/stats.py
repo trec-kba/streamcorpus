4c83b56ffb6f98624213cb448367a4f085dbdf48
stephanie <swang93@diffeo.com>
2014-01-14 19:25:18 +0000

    still very slow...looks like it's because of for-looping through StreamItems in Chunk added support for other Token.___Types, now has --entity-type and --mention-type options

--- py/src/streamcorpus/stats.py
+++ py/src/streamcorpus/stats.py
@@ -27,30 +27,36 @@ versioned_classes = {
     'v0_1_0': StreamItem_v0_1_0,
     }
 
-def _count_entity_types(fpath, entity_types, message_class):
+filter_classes = {
+    'entity_type' : EntityType,
+    'mention_type' : MentionType,
+}
+
+def _count_filter_types(fpath, filter_by, filter_values, message_class):
     all_counter = collections.defaultdict(collections.Counter)
 
     for num, si in enumerate(Chunk(path=fpath, mode='rb', message=message_class)):
         print 'counting entity types for StreamItem', num
         if si.body.sentences:
             for tagger_id, sentences in si.body.sentences.items():
-                sentences_counter = collections.Counter(_token_iter(sentences, entity_types))
+                sentences_counter = collections.Counter(_filter_token_attr(sentences, filter_by, filter_values))
                 all_counter[tagger_id].update(sentences_counter)
                 print '\t counting entity types for tagger id', tagger_id
-                for entity_type in entity_types:
-                    print '\t\t', EntityType._VALUES_TO_NAMES[entity_type], sentences_counter[entity_type]
+                for filter_value in filter_values:
+                    print '\t\t', filter_classes[filter_by]._VALUES_TO_NAMES[filter_value], sentences_counter[filter_value]
 
     print 'counting entity types for all StreamItems'
     for tagger_id, counter in all_counter.items():
         print '\t', tagger_id
-        for entity_type in entity_types:
-            print '\t\t', EntityType._VALUES_TO_NAMES[entity_type], counter[entity_type]
+        for filter_value in filter_values:
+            print '\t\t', filter_classes[filter_by]._VALUES_TO_NAMES[filter_value], counter[filter_value]
 
-def _token_iter(sentences, entity_types):
+def _filter_token_attr(sentences, attr, filter_types):
     for sentence in sentences:
         for tok in sentence.tokens:
-            if tok.entity_type in entity_types:
-                yield tok.entity_type
+            value = getattr(tok, attr)
+            if value:
+                yield getattr(tok, attr)
 
 def main():
     logger = logging.getLogger('streamcorpus')
@@ -66,10 +72,16 @@ def main():
     )
     parser.add_argument('--version', default='v0_3_0')
     parser.add_argument(
-        '--count-entity-types',
-        dest='count_entity_types',
+        '--entity-type',
+        dest='entity_type',
         nargs='*',
         help='Return count of all tokens of specified types. Default is all types'
+    )
+    parser.add_argument(
+        '--mention-type',
+        dest='mention_type',
+        nargs='*',
+        help='Return count of all tokens of specified mention types. Default is all mention types'
     )
     args = parser.parse_args()
 
@@ -92,13 +104,15 @@ def main():
             paths.append(ipath)
     args.input_path = paths
     
-    if args.count_entity_types is not None:
-        if len(args.count_entity_types) == 0:
-            count_entity_types = EntityType._VALUES_TO_NAMES.keys()
-        else:
-            count_entity_types = [EntityType._NAMES_TO_VALUES[name] for name in args.count_entity_types]
-        for fpath in args.input_path:
-            _count_entity_types(fpath, count_entity_types, message_class)         
+    for filter_class in filter_classes:
+        arg = getattr(args, filter_class)
+        if arg is not None:
+            if len(arg) == 0:
+                filter_values = filter_classes[filter_class]._VALUES_TO_NAMES.keys()
+            else:
+                filter_values = [filter_classes[filter_class]._NAMES_TO_VALUES[name] for name in arg]
+            for fpath in args.input_path:
+                _count_filter_types(fpath, filter_class, filter_values, message_class)
 
 if __name__ == '__main__':
     main()
