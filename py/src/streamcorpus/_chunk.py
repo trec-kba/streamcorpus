dc670a22a525331914161115f4480081cdc871bc
David Maze <dmaze@diffeo.com>
2014-04-18 17:02:53 +0000

    minor docstring syntax fix

--- py/src/streamcorpus/_chunk.py
+++ py/src/streamcorpus/_chunk.py
@@ -406,8 +406,8 @@ def decrypt_and_uncompress(data, gpg_private=None, tmp_dir='/tmp'):
     Given a data buffer of bytes, if gpg_key_path is provided, decrypt
     data using gnupg, and uncompress using xz.
 
-    :returns (logs, data): where logs is an array of strings, and data
-    is a binary string.
+    :returns: a tuple of (logs, data), where `logs` is an array of
+      strings and data is a binary string
     '''
     _errors = []
     tmp_path = os.path.join(tmp_dir, 'tmp-compress-and-encrypt-path-' + uuid.uuid4().hex)
