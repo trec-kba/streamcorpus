6838ff8916dbc2396106b9a9b8248f31ba337029
tom <tom@diffeo.com>
2015-03-30 16:24:18 +0000

    adding Selector to package_globals

--- py/src/streamcorpus/package_globals.py
+++ py/src/streamcorpus/package_globals.py
@@ -33,15 +33,12 @@ in streamcorpus.thrift
    Copyright 2012-2014 Diffeo, Inc.
 '''
 
-import re
-from time import mktime
 import time
 import hashlib
 from datetime import datetime
-from cStringIO import StringIO
 
 ## import the KBA-specific thrift types
-from ttypes import StreamItem, ContentItem, Label, StreamTime, \
+from streamcorpus.ttypes import StreamItem, ContentItem, Label, StreamTime, Selector, \
     Offset, Rating, Annotator, Versions, Token, Sentence, EntityType, \
     Tagging, OffsetType, Target, \
     Language, \
@@ -51,18 +48,18 @@ from ttypes import StreamItem, ContentItem, Label, StreamTime, \
 ## unambiguous name for the current one
 StreamItem_v0_3_0 = StreamItem
 
-from ttypes_v0_1_0 import StreamItem as StreamItem_v0_1_0
-from ttypes_v0_2_0 import StreamItem as StreamItem_v0_2_0
-import ttypes_v0_1_0
-import ttypes_v0_2_0
+from streamcorpus.ttypes_v0_1_0 import StreamItem as StreamItem_v0_1_0
+from streamcorpus.ttypes_v0_2_0 import StreamItem as StreamItem_v0_2_0
+import streamcorpus.ttypes_v0_1_0 as ttypes_v0_1_0
+import streamcorpus.ttypes_v0_2_0 as ttypes_v0_2_0
 
-from _chunk import Chunk, PickleChunk, JsonChunk, \
+from streamcorpus._chunk import Chunk, PickleChunk, JsonChunk, \
     parse_file_extensions, known_compression_schemes, \
     decrypt_and_uncompress, compress_and_encrypt, \
     compress_and_encrypt_path, \
     serialize, deserialize, \
     VersionMismatchError
-from _cbor_chunk import CborChunk
+from streamcorpus._cbor_chunk import CborChunk
 
 __all__ = ['Chunk', 'PickleChunk', 'JsonChunk', 'CborChunk',
            'decrypt_and_uncompress', 'compress_and_encrypt',
@@ -74,7 +71,7 @@ __all__ = ['Chunk', 'PickleChunk', 'JsonChunk', 'CborChunk',
            'get_entity_type',
            'get_date_hour',
            'add_annotation',
-           'StreamItem', 'ContentItem', 'Label', 'StreamTime',
+           'StreamItem', 'ContentItem', 'Label', 'StreamTime', 'Selector',
            'Offset', 'Rating', 'Annotator', 'Versions', 'Token', 'Sentence', 'EntityType',
            'Target',
            'Tagging', 'OffsetType',
